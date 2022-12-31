4d5b3a2ef2de10ca680553049d01dc55972c7797
Eleonardo Oliveira <eleonardo.ro@gmail.com>
2022-12-30 23:34:58 -0300

    - incrementing inputs with console.log validation

--- src/Formulario.js
+++ src/Formulario.js
@@ -3,9 +3,9 @@ import React from 'react'
 class Formulario extends React.Component {
     render() {
         return (
-            <form>
-                <input type='text' placeholder='Digite o nome' className='form-control'/>
-                <input type='number' placeholder='Digite a idade' className='form-control'/>
+            <form onSubmit={this.props.funcaoBotao}>
+                <input type='text' placeholder='Digite o nome' className='form-control' name='nome' onChange={this.props.funcaoCampo}/>
+                <input type='number' placeholder='Digite a idade' className='form-control' name='idade' onChange={this.props.funcaoCampo}/>
                 <input type='submit' value='Cadastrar' className='btn btn-primary'/>
             </form>
         )
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-import './estilos.css'
-import Formulario from './Formulario'
+import "./estilos.css";
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
 
 class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                <Formulario/>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nome: "",
+      idade: null,
+    };
+  }
+
+  aoDigitar = (campo) => {
+    this.setState({ [campo.target.name]: campo.target.value });
+  };
+
+  aoClicar = (botao) => {
+    botao.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <Formulario funcaoBotao={this.aoClicar} funcaoCampo={this.aoDigitar} />
+        <Tabela />
+      </div>
+    );
+  }
 }
 
-ReactDom.render(<Main />, document.getElementById('root'))
+ReactDom.render(<Main />, document.getElementById("root"));
